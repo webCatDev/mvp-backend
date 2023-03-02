d4a3e9c2d6e35fa1b750a7da6af6e638e2dd9aa4
Hamza Eşsiz <webcatdev@gmail.com>
2023-03-02 16:48:18 +0300

    allow cors

--- app.js
+++ app.js
@@ -1,36 +1,29 @@
-const express = require("express")
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const app = express();
 
 // ROUTES
-const rootRouter = require('./routes/root.js')
-const authRouter = require('./routes/auth.js')
-const mvpRouter = require('./routes/mvp.js')
-const imagesRouter = require('./routes/images.js')
-const fieldsRouter = require('./routes/fields.js')
-const contributionsRouter = require('./routes/contributions.js')
-
+const rootRouter = require("./routes/root.js");
+const authRouter = require("./routes/auth.js");
+const mvpRouter = require("./routes/mvp.js");
+const imagesRouter = require("./routes/images.js");
+const fieldsRouter = require("./routes/fields.js");
+const contributionsRouter = require("./routes/contributions.js");
 
 // MIDDLEWARES
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    optionsSuccessStatus: 200,
-    credentials: true,
-  })
-);
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(express.static('public'))
-app.use(cookieParser())
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // ROUTE MIDDLEWARES
-app.use("/", rootRouter)
-app.use("/auth", authRouter)
-app.use('/images', imagesRouter)
-app.use('/fields', fieldsRouter)
-app.use('/mvps', mvpRouter)
-app.use('/contributions', contributionsRouter)
+app.use("/", rootRouter);
+app.use("/auth", authRouter);
+app.use("/images", imagesRouter);
+app.use("/fields", fieldsRouter);
+app.use("/mvps", mvpRouter);
+app.use("/contributions", contributionsRouter);
 
-module.exports = app
+module.exports = app;
